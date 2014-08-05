b1dce20a1309e4ea9e09b87f7a1201560af886eb
Simon Arneson <simon@cymonit.se>
2014-08-05 09:52:02 +0000

    first commit

---
+++ client/js/modules/combination.js
@@ -0,0 +1,7 @@
+var COMBINATION = (function() {
+    my.newCombination(c1,c2,typeOfComb){
+      return {criteria1:c1, criteria2:c2,type:typeOfComb};  
+    };
+    
+    return my;
+}());
---
+++ client/js/modules/critera.js
@@ -0,0 +1,7 @@
+var CRITERIA = (function() {
+    my.newCriteria(critName,critType,critCombination){
+      return {name:critName,type:critType,combination:critCombination};  
+    };
+    
+    return my;
+}());
